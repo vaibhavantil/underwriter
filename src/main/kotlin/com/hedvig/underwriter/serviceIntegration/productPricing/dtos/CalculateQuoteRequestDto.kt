da5529c6907ab34b59ebb856a90312b4e9a829d5
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2019-12-09 17:14:03 +0100

    get rid of redundant dtos, due to refactored signing process in PP

--- src/main/kotlin/com/hedvig/underwriter/serviceIntegration/productPricing/dtos/CalculateQuoteRequestDto.kt
+++ src/main/kotlin/com/hedvig/underwriter/serviceIntegration/productPricing/dtos/CalculateQuoteRequestDto.kt
@@ -1,10 +1,5 @@
 package com.hedvig.underwriter.serviceIntegration.productPricing.dtos
 
-import com.hedvig.underwriter.model.ApartmentData
-import com.hedvig.underwriter.model.ApartmentProductSubType
-import com.hedvig.underwriter.model.HouseData
-import com.hedvig.underwriter.model.Quote
-import com.hedvig.underwriter.model.birthDateFromSsn
 import java.time.LocalDate
 
 data class CalculateQuoteRequestDto(
@@ -24,73 +19,4 @@ data class CalculateQuoteRequestDto(
     val numberOfBathrooms: Int?,
     val extraBuildings: List<ExtraBuildingDTO>?,
     val isSubleted: Boolean?
-) {
-    companion object {
-        fun from(quote: Quote): CalculateQuoteRequestDto {
-            return when (quote.data) {
-                is ApartmentData -> CalculateQuoteRequestDto(
-                    memberId = quote.memberId!!,
-                    ssn = quote.data.ssn!!,
-                    firstName = quote.data.firstName!!,
-                    lastName = quote.data.lastName!!,
-                    birthDate = quote.data.ssn!!.birthDateFromSsn(),
-                    student = quote.data.isStudent,
-                    address = Address(
-                        quote.data.street!!,
-                        quote.data.city,
-                        quote.data.zipCode!!,
-                        0
-                    ),
-                    livingSpace = quote.data.livingSpace!!.toFloat(),
-                    houseType = when (quote.data.subType!!) {
-                        ApartmentProductSubType.BRF -> ProductPricingProductTypes.BRF
-                        ApartmentProductSubType.RENT -> ProductPricingProductTypes.RENT
-                        ApartmentProductSubType.RENT_BRF -> TODO() // what is even rent brf?
-                        ApartmentProductSubType.SUBLET_RENTAL -> ProductPricingProductTypes.STUDENT_RENT
-                        ApartmentProductSubType.SUBLET_BRF -> ProductPricingProductTypes.STUDENT_BRF
-                        ApartmentProductSubType.STUDENT_BRF -> ProductPricingProductTypes.STUDENT_BRF
-                        ApartmentProductSubType.STUDENT_RENT -> ProductPricingProductTypes.STUDENT_RENT
-                        ApartmentProductSubType.LODGER -> TODO()
-                        ApartmentProductSubType.UNKNOWN -> TODO()
-                    },
-                    currentInsurer = quote.currentInsurer,
-                    personsInHouseHold = quote.data.householdSize!!,
-                    ancillaryArea = null,
-                    yearOfConstruction = null,
-                    numberOfBathrooms = null,
-                    extraBuildings = null,
-                    isSubleted = null
-                )
-                is HouseData -> CalculateQuoteRequestDto(
-                    memberId = quote.memberId!!,
-                    ssn = quote.data.ssn!!,
-                    firstName = quote.data.firstName!!,
-                    lastName = quote.data.lastName!!,
-                    birthDate = quote.data.ssn!!.birthDateFromSsn(),
-                    student = false,
-                    address = Address(
-                        quote.data.street!!,
-                        quote.data.city,
-                        quote.data.zipCode!!,
-                        0
-                    ),
-                    livingSpace = quote.data.livingSpace!!.toFloat(),
-                    houseType = ProductPricingProductTypes.HOUSE,
-                    currentInsurer = quote.currentInsurer,
-                    personsInHouseHold = quote.data.householdSize!!,
-                    ancillaryArea = quote.data.ancillaryArea,
-                    yearOfConstruction = quote.data.yearOfConstruction,
-                    numberOfBathrooms = quote.data.numberOfBathrooms,
-                    extraBuildings = quote.data.extraBuildings?.map { extraBuilding ->
-                        ExtraBuildingDTO(
-                            type = com.hedvig.underwriter.graphql.type.ExtraBuildingType.valueOf(extraBuilding.type.name),
-                            area = extraBuilding.area,
-                            hasWaterConnected = extraBuilding.hasWaterConnected
-                        )
-                    },
-                    isSubleted = quote.data.isSubleted
-                )
-            }
-        }
-    }
-}
+)
--- src/main/kotlin/com/hedvig/underwriter/serviceIntegration/productPricing/dtos/RapioQuoteRequestDto.kt
+++ src/main/kotlin/com/hedvig/underwriter/serviceIntegration/productPricing/dtos/RapioQuoteRequestDto.kt
@@ -1,12 +1,7 @@
 package com.hedvig.underwriter.serviceIntegration.productPricing.dtos
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.hedvig.underwriter.model.ApartmentData
-import com.hedvig.underwriter.model.ApartmentProductSubType
-import com.hedvig.underwriter.model.HouseData
-import com.hedvig.underwriter.model.Quote
 import com.hedvig.underwriter.model.QuoteInitiatedFrom
-import com.hedvig.underwriter.model.birthDateFromSsn
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -34,86 +29,4 @@ data class RapioQuoteRequestDto(
     val extraBuildings: List<ExtraBuildingDTO>?,
     @get:JsonProperty("isSubleted")
     val isSubleted: Boolean?
-) {
-    companion object {
-        fun from(quote: Quote, email: String): RapioQuoteRequestDto {
-            return when (val data = quote.data) {
-                is HouseData -> {
-                    RapioQuoteRequestDto(
-                        quote.price!!,
-                        data.firstName!!,
-                        data.lastName!!,
-                        data.ssn!!.birthDateFromSsn(),
-                        false,
-                        Address(
-                            data.street!!,
-                            data.city!!,
-                            data.zipCode!!,
-                            0
-                        ),
-                        data.livingSpace!!.toFloat(),
-                        ProductPricingProductTypes.HOUSE,
-                        quote.currentInsurer,
-                        data.householdSize!!,
-                        quote.startDate?.atStartOfDay(),
-                        data.ssn,
-                        email,
-                        "",
-                        quote.initiatedFrom,
-                        data.ancillaryArea,
-                        data.yearOfConstruction,
-                        data.numberOfBathrooms,
-                        data.extraBuildings?.map { extraBuilding ->
-                            ExtraBuildingDTO(
-                                com.hedvig.underwriter.graphql.type.ExtraBuildingType.valueOf(extraBuilding.type.name),
-                                extraBuilding.area,
-                                extraBuilding.hasWaterConnected
-                            )
-                        },
-                        data.isSubleted
-                    )
-                }
-                is ApartmentData -> {
-                    RapioQuoteRequestDto(
-                        quote.price!!,
-                        data.firstName!!,
-                        data.lastName!!,
-                        data.ssn!!.birthDateFromSsn(),
-                        false,
-                        Address(
-                            data.street!!,
-                            data.city!!,
-                            data.zipCode!!,
-                            0
-                        ),
-                        data.livingSpace!!.toFloat(),
-                        when (data.subType!!) {
-                            ApartmentProductSubType.BRF -> ProductPricingProductTypes.BRF
-                            ApartmentProductSubType.RENT -> ProductPricingProductTypes.RENT
-                            ApartmentProductSubType.RENT_BRF -> TODO()
-                            ApartmentProductSubType.SUBLET_RENTAL -> ProductPricingProductTypes.STUDENT_RENT
-                            ApartmentProductSubType.SUBLET_BRF -> ProductPricingProductTypes.STUDENT_BRF
-                            ApartmentProductSubType.STUDENT_BRF -> ProductPricingProductTypes.STUDENT_BRF
-                            ApartmentProductSubType.STUDENT_RENT -> ProductPricingProductTypes.STUDENT_RENT
-                            ApartmentProductSubType.LODGER -> TODO()
-                            ApartmentProductSubType.UNKNOWN -> TODO()
-                        },
-                        quote.currentInsurer,
-                        data.householdSize!!,
-                        quote.startDate?.atStartOfDay(),
-                        data.ssn,
-                        email,
-                        "",
-                        quote.initiatedFrom,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null
-                    )
-                }
-                else -> throw RuntimeException("Incomplete quote is of unknown type: ${quote.data::class}")
-            }
-        }
-    }
-}
+)
