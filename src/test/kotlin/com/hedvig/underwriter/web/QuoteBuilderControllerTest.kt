d175f9fe723c36a25e1387fc97d1801fd11e404d
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2019-12-10 14:58:07 +0100

    amend test

--- src/test/kotlin/com/hedvig/underwriter/web/QuoteBuilderControllerTest.kt
+++ src/test/kotlin/com/hedvig/underwriter/web/QuoteBuilderControllerTest.kt
@@ -44,7 +44,6 @@ internal class QuoteBuilderControllerTest {
             "dateStartedRecievingQuoteInfo": "2019-09-17T13:32:00.783981Z",
             "apartmentProductSubType": "RENT",
             "IncompleteQuoteData": {
-                "type": "apartment",
                 "incompleteApartmentQuoteData": {
                     "zipcode": "11216"
                 }
