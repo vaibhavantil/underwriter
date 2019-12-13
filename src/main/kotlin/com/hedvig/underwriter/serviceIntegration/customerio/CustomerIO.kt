42c6aec4746eab844c0c3b59e77d51a6d87a8dae
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2019-12-13 13:03:59 +0100

    add sentry logging

--- src/main/kotlin/com/hedvig/underwriter/serviceIntegration/customerio/CustomerIO.kt
+++ src/main/kotlin/com/hedvig/underwriter/serviceIntegration/customerio/CustomerIO.kt
@@ -1,23 +1,31 @@
 package com.hedvig.underwriter.serviceIntegration.customerio
 
 import com.hedvig.underwriter.model.Partner
+import io.sentry.Sentry
 import mu.KotlinLogging
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.stereotype.Component
+import org.springframework.core.env.Environment
+import java.util.*
 
 private val logger = KotlinLogging.logger {}
 
 @ConditionalOnProperty(value = ["customerio.username"], matchIfMissing = false)
 @Component
 @EnableFeignClients
-class CustomerIO(val customerIOClient: CustomerIOClient) {
+class CustomerIO(val customerIOClient: CustomerIOClient, val env: Environment) {
 
     fun setPartnerCode(memberId: String, partner: Partner) {
         try {
             val map = mutableMapOf("partner_code" to partner.name)
             this.customerIOClient.put(memberId, map)
         } catch (ex: Exception) {
+            if (Arrays.stream(env.activeProfiles).anyMatch { env -> env.contentEquals("staging")
+                    || env.contentEquals("production") }
+            ) {
+                Sentry.capture(ex)
+            }
             logger.error("Could not assign \"partner_code\" to \"${partner.name}\" for $memberId", ex)
         }
     }
