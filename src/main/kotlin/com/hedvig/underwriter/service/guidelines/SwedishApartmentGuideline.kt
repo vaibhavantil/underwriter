28f14f1a75c97e064de3d8c300bec3a22827dd81
meletios <meletios@hedvig.com>
2020-02-05 14:37:20 +0100

    rename classes

--- src/main/kotlin/com/hedvig/underwriter/service/guidelines/SwedishApartmentGuideline.kt
+++ src/main/kotlin/com/hedvig/underwriter/service/guidelines/SwedishApartmentGuideline.kt
@@ -1,43 +1,43 @@
 package com.hedvig.underwriter.service.guidelines
 
-import com.hedvig.underwriter.model.ApartmentData
 import com.hedvig.underwriter.model.ApartmentProductSubType
+import com.hedvig.underwriter.model.SwedishApartmentData
 import com.hedvig.underwriter.model.birthDateFromSsn
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
-interface SwedishApartmentGuideline : BaseGuideline<ApartmentData>
+interface SwedishApartmentGuideline : BaseGuideline<SwedishApartmentData>
 
 class SwedishApartmentHouseHoldSizeAtLeast1 : SwedishApartmentGuideline {
     override val errorMessage: String = "breaches underwriting guideline household size, must be at least 1"
 
-    override val validate = { data: ApartmentData -> data.householdSize!! < 1 }
+    override val validate = { data: SwedishApartmentData -> data.householdSize!! < 1 }
 }
 
 class SwedishApartmentLivingSpaceAtLeast1Sqm : SwedishApartmentGuideline {
     override val errorMessage: String = "breaches underwriting guideline living space, must be at least 1 sqm"
 
-    override val validate = { data: ApartmentData -> data.livingSpace!! < 1 }
+    override val validate = { data: SwedishApartmentData -> data.livingSpace!! < 1 }
 }
 
 class SwedishApartmentHouseHoldSizeNotMoreThan6 : SwedishApartmentGuideline {
     override val errorMessage: String = "breaches underwriting guideline household size must be less than or equal to 6"
 
-    override val validate = { data: ApartmentData -> data.householdSize!! > 6 }
+    override val validate = { data: SwedishApartmentData -> data.householdSize!! > 6 }
 }
 
 class SwedishApartmentLivingSpaceNotMoreThan250Sqm : SwedishApartmentGuideline {
     override val errorMessage: String =
         "breaches underwriting guideline living space must be less than or equal to 250 sqm"
 
-    override val validate = { data: ApartmentData -> data.livingSpace!! > 250 }
+    override val validate = { data: SwedishApartmentData -> data.livingSpace!! > 250 }
 }
 
 class SwedishStudentApartmentHouseholdSizeNotMoreThan2 : SwedishApartmentGuideline {
     override val errorMessage: String = "breaches underwriting guideline household size must be less than 2"
 
     override val validate =
-        { data: ApartmentData ->
+        { data: SwedishApartmentData ->
             (data.subType == ApartmentProductSubType.STUDENT_RENT || data.subType == ApartmentProductSubType.STUDENT_BRF) &&
                 data.householdSize!! > 2
         }
@@ -48,7 +48,7 @@ class SwedishStudentApartmentLivingSpaceNotMoreThan50Sqm : SwedishApartmentGuide
         "breaches underwriting guideline living space must be less than or equal to 50sqm"
 
     override val validate =
-        { data: ApartmentData ->
+        { data: SwedishApartmentData ->
             (data.subType == ApartmentProductSubType.STUDENT_RENT || data.subType == ApartmentProductSubType.STUDENT_BRF) &&
                 data.livingSpace!! > 50
         }
@@ -59,11 +59,11 @@ class SwedishStudentApartmentAgeNotMoreThan30Years : SwedishApartmentGuideline {
         "breaches underwriting guidelines member must be 30 years old or younger"
 
     override val validate =
-        { data: ApartmentData ->
+        { data: SwedishApartmentData ->
             (data.subType == ApartmentProductSubType.STUDENT_RENT || data.subType == ApartmentProductSubType.STUDENT_BRF) &&
                 data.ssn!!.birthDateFromSsn().until(
-                LocalDate.now(),
-                ChronoUnit.YEARS
-            ) > 30
+                    LocalDate.now(),
+                    ChronoUnit.YEARS
+                ) > 30
         }
 }
--- src/main/kotlin/com/hedvig/underwriter/service/guidelines/SwedishHouseGuideline.kt
+++ src/main/kotlin/com/hedvig/underwriter/service/guidelines/SwedishHouseGuideline.kt
@@ -1,45 +1,45 @@
 package com.hedvig.underwriter.service.guidelines
 
-import com.hedvig.underwriter.model.HouseData
+import com.hedvig.underwriter.model.SwedishHouseData
 
-interface SwedishHouseGuideline : BaseGuideline<HouseData>
+interface SwedishHouseGuideline : BaseGuideline<SwedishHouseData>
 
 class SwedishHouseHouseholdSizeAtLeast1 : SwedishHouseGuideline {
     override val errorMessage: String = "breaches underwriting guideline household size, must be at least 1"
 
-    override val validate = { data: HouseData -> data.householdSize!! < 1 }
+    override val validate = { data: SwedishHouseData -> data.householdSize!! < 1 }
 }
 
 class SwedishHouseLivingSpaceAtLeast1Sqm : SwedishHouseGuideline {
     override val errorMessage: String = "breaches underwriting guideline living space, must be at least 1 sqm"
 
-    override val validate = { data: HouseData -> data.livingSpace!! < 1 }
+    override val validate = { data: SwedishHouseData -> data.livingSpace!! < 1 }
 }
 
 class SwedishHouseHouseholdSizeNotMoreThan6 : SwedishHouseGuideline {
     override val errorMessage: String = "breaches underwriting guideline household size, must not be more than 6"
 
-    override val validate = { data: HouseData -> data.householdSize!! > 6 }
+    override val validate = { data: SwedishHouseData -> data.householdSize!! > 6 }
 }
 
 class SwedishHouseLivingSpaceNotMoreThan250Sqm : SwedishHouseGuideline {
     override val errorMessage: String = "breaches underwriting guideline living space, must not be more than 250 sqm"
 
-    override val validate = { data: HouseData -> data.livingSpace!! > 250 }
+    override val validate = { data: SwedishHouseData -> data.livingSpace!! > 250 }
 }
 
 class SwedishHouseYearOfConstruction : SwedishHouseGuideline {
     override val errorMessage: String =
         "breaches underwriting guideline year of construction, must not be older than 1925"
 
-    override val validate = { data: HouseData -> data.yearOfConstruction!! < 1925 }
+    override val validate = { data: SwedishHouseData -> data.yearOfConstruction!! < 1925 }
 }
 
 class SwedishHouseNumberOfBathrooms : SwedishHouseGuideline {
     override val errorMessage: String =
         "breaches underwriting guideline number of bathrooms, must not be more than 2"
 
-    override val validate = { data: HouseData -> data.numberOfBathrooms!! > 2 }
+    override val validate = { data: SwedishHouseData -> data.numberOfBathrooms!! > 2 }
 }
 
 class SwedishHouseNumberOfExtraBuildingsWithAreaOverSixSqm : SwedishHouseGuideline {
@@ -47,7 +47,7 @@ class SwedishHouseNumberOfExtraBuildingsWithAreaOverSixSqm : SwedishHouseGuideli
         "breaches underwriting guideline extra building areas, number of extra buildings with an area over 6 sqm must not be more than 4"
 
     override val validate =
-        { data: HouseData -> data.extraBuildings!!.filter { building -> building.area > 6 }.size > 4 }
+        { data: SwedishHouseData -> data.extraBuildings!!.filter { building -> building.area > 6 }.size > 4 }
 }
 
 class SwedishHouseExtraBuildingsSizeNotOverThan75Sqm : SwedishHouseGuideline {
@@ -55,7 +55,7 @@ class SwedishHouseExtraBuildingsSizeNotOverThan75Sqm : SwedishHouseGuideline {
         "breaches underwriting guideline extra building areas, extra buildings may not be over 75 sqm"
 
     override val validate =
-        { data: HouseData -> data.extraBuildings!!.any { building -> building.area > 75 } }
+        { data: SwedishHouseData -> data.extraBuildings!!.any { building -> building.area > 75 } }
 }
 
 class SwedishHouseExtraBuildingsSizeAtLeast1Sqm : SwedishHouseGuideline {
@@ -63,5 +63,5 @@ class SwedishHouseExtraBuildingsSizeAtLeast1Sqm : SwedishHouseGuideline {
         "breaches underwriting guideline extra building areas, extra buildings must have an area of at least 1"
 
     override val validate =
-        { data: HouseData -> data.extraBuildings!!.any { building -> building.area < 1 } }
+        { data: SwedishHouseData -> data.extraBuildings!!.any { building -> building.area < 1 } }
 }
