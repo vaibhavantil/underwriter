c58286afbbb24413eaa67861a3d3eaaeb53d464c
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-02-10 10:47:44 +0100

    tavel dose not have address

--- src/main/kotlin/com/hedvig/underwriter/serviceIntegration/memberService/dtos/FinalizeOnBoardingRequest.kt
+++ src/main/kotlin/com/hedvig/underwriter/serviceIntegration/memberService/dtos/FinalizeOnBoardingRequest.kt
@@ -1,7 +1,11 @@
 package com.hedvig.underwriter.serviceIntegration.memberService.dtos
 
 import com.hedvig.underwriter.model.AddressData
+import com.hedvig.underwriter.model.NorwegianHomeContentsData
+import com.hedvig.underwriter.model.NorwegianTravelData
 import com.hedvig.underwriter.model.Quote
+import com.hedvig.underwriter.model.SwedishApartmentData
+import com.hedvig.underwriter.model.SwedishHouseData
 import com.hedvig.underwriter.service.model.PersonPolicyHolder
 
 class FinalizeOnBoardingRequest(
@@ -11,7 +15,7 @@ class FinalizeOnBoardingRequest(
     val lastName: String,
     val email: String,
     val phoneNumber: String?,
-    val address: Address
+    val address: Address?
 ) {
     companion object {
 
@@ -19,25 +23,40 @@ class FinalizeOnBoardingRequest(
             quote: Quote,
             email: String,
             phoneNumber: String? = null
-        ): FinalizeOnBoardingRequest {
-            val addressInsurance = quote.data as AddressData
-            val personPolicyHolder = quote.data as PersonPolicyHolder<*>
+        ): FinalizeOnBoardingRequest =
+            when (quote.data) {
+                is SwedishHouseData,
+                is SwedishApartmentData,
+                is NorwegianHomeContentsData -> {
+                    val addressInsurance = quote.data as AddressData
+                    val personPolicyHolder = quote.data as PersonPolicyHolder<*>
 
-            return FinalizeOnBoardingRequest(
-                memberId = quote.memberId!!,
-                ssn = personPolicyHolder.ssn!!,
-                firstName = personPolicyHolder.firstName!!,
-                lastName = personPolicyHolder.lastName!!,
-                email = email,
-                phoneNumber = phoneNumber,
-                address = Address(
-                    street = addressInsurance.street!!,
-                    city = addressInsurance.city ?: "",
-                    zipCode = addressInsurance.zipCode!!,
-                    apartmentNo = "",
-                    floor = 0
-                )
-            )
-        }
+                    FinalizeOnBoardingRequest(
+                        memberId = quote.memberId!!,
+                        ssn = personPolicyHolder.ssn!!,
+                        firstName = personPolicyHolder.firstName!!,
+                        lastName = personPolicyHolder.lastName!!,
+                        email = email,
+                        phoneNumber = phoneNumber,
+                        address = Address(
+                            street = addressInsurance.street!!,
+                            city = addressInsurance.city ?: "",
+                            zipCode = addressInsurance.zipCode!!,
+                            apartmentNo = "",
+                            floor = 0
+                        )
+                    )
+                }
+                is NorwegianTravelData ->
+                    FinalizeOnBoardingRequest(
+                        memberId = quote.memberId!!,
+                        ssn = quote.data.ssn!!,
+                        firstName = quote.data.firstName!!,
+                        lastName = quote.data.lastName!!,
+                        email = email,
+                        phoneNumber = phoneNumber,
+                        address = null
+                    )
+            }
     }
 }
