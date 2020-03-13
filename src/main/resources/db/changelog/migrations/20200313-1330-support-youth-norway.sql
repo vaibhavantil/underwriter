15a52d3d876afcf222656573d095419da6cf0079
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-03-13 15:16:06 +0100

    found a small thing

--- src/main/resources/db/changelog/migrations/20200313-1330-support-youth-norway.sql
+++ src/main/resources/db/changelog/migrations/20200313-1330-support-youth-norway.sql
@@ -8,4 +8,4 @@ ALTER TABLE quote_revision_norwegian_home_contents_data
 ALTER TABLE quote_revision_norwegian_travel_data
     ADD COLUMN is_youth boolean NOT NULL DEFAULT false;
 
---rollback ALTER TABLE quote_revision_norwegian_home_contents_data RENAME COLUMN is_student TO is_youth; ALTER TABLE quote_revision_norwegian_travel_data DROP COLUMN is_youth;
+--rollback ALTER TABLE quote_revision_norwegian_home_contents_data RENAME COLUMN is_youth TO is_student; ALTER TABLE quote_revision_norwegian_travel_data DROP COLUMN is_youth;
