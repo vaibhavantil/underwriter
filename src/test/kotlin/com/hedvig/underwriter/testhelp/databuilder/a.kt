203705612a03715a7d219d37f9cdad296ad35fb0
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-04-02 15:15:28 +0200

    ssn

--- src/test/kotlin/com/hedvig/underwriter/testhelp/databuilder/a.kt
+++ src/test/kotlin/com/hedvig/underwriter/testhelp/databuilder/a.kt
@@ -191,7 +191,7 @@ class a {
 
     data class NorwegianTravelDataBuilder(
         val id: UUID = UUID.fromString("ab5924e4-0c72-11ea-a337-4865ee119be4"),
-        val ssn: String? = "191212121212",
+        val ssn: String? = "12121200000",
         val birthDate: LocalDate = LocalDate.of(1912, 12, 12),
         val firstName: String = "",
         val lastName: String = "",
