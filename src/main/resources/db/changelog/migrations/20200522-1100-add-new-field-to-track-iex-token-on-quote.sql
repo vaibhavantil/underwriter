027fc624c71d01a5b60c9abadbadc6787cf90b0c
meletios <meletios@hedvig.com>
2020-05-22 13:16:56 +0200

    convert it to text

--- src/main/resources/db/changelog/migrations/20200522-1100-add-new-field-to-track-iex-token-on-quote.sql
+++ src/main/resources/db/changelog/migrations/20200522-1100-add-new-field-to-track-iex-token-on-quote.sql
@@ -3,7 +3,7 @@
 --changeset meletis:20200522-1100-add-new-field-to-track-iex-token-on-quote.sql
 
 ALTER TABLE quote_revisions
-    ADD COLUMN sign_from_hope_triggered_by varchar(255) NULL
+    ADD COLUMN sign_from_hope_triggered_by text NULL
 ;
 
 --rollback ALTER TABLE quote_revisions DROP COLUMN sign_from_hope_triggered_by;
