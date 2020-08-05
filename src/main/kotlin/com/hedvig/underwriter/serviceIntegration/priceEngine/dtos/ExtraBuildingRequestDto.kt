996f76507094436ec4cda42c68e35ada708e1c5a
meletios <meletios@hedvig.com>
2020-08-05 17:48:14 +0300

    move xtraBuildingRequestDto  under priceEngine

---
+++ src/main/kotlin/com/hedvig/underwriter/serviceIntegration/priceEngine/dtos/ExtraBuildingRequestDto.kt
@@ -0,0 +1,22 @@
+package com.hedvig.underwriter.serviceIntegration.priceEngine.dtos
+
+import com.hedvig.underwriter.model.ExtraBuildingType
+import com.hedvig.underwriter.serviceIntegration.productPricing.dtos.contract.ExtraBuildingDto
+import java.util.UUID
+
+data class ExtraBuildingRequestDto(
+    val id: UUID?,
+    val type: ExtraBuildingType,
+    val area: Int,
+    val hasWaterConnected: Boolean
+) {
+    companion object {
+        fun from(extraBuildingDto: ExtraBuildingDto) =
+            ExtraBuildingRequestDto(
+                id = null,
+                type = extraBuildingDto.type,
+                area = extraBuildingDto.area,
+                hasWaterConnected = extraBuildingDto.hasWaterConnected
+            )
+    }
+}
