a6e0c97a408b3b0689ac573fbaea0b4707d70b42
vonElfvin <elvin.granat@gmail.com>
2020-11-02 16:48:55 +0100

    Add default to is_student and type

--- src/main/resources/db/changelog/migrations/20201028-1015-support-student-and-type-denmark.sql
+++ src/main/resources/db/changelog/migrations/20201028-1015-support-student-and-type-denmark.sql
@@ -3,9 +3,9 @@
 --changeset elvingranat:20201028-1015-support-student-and-type-denmark.sql
 
 ALTER TABLE quote_revision_danish_home_contents_data
-    ADD COLUMN is_student boolean NOT NULL;
+    ADD COLUMN is_student boolean NOT NULL DEFAULT false;
 
 ALTER TABLE quote_revision_danish_home_contents_data
-    ADD COLUMN type VARCHAR (20) NOT NULL;
+    ADD COLUMN type VARCHAR (20) NOT NULL DEFAULT 'RENT';
 
 --rollback ALTER TABLE quote_revision_danish_home_contents_data DROP COLUMN is_student; ALTER TABLE quote_revision_danish_home_contents_data DROP COLUMN type;
