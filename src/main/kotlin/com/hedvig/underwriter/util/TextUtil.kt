015f36c2b47c32bf1d7c3243abea3c2260850afb
vonElfvin <elvin.granat@gmail.com>
2020-11-16 12:27:48 +0100

    Minor refactor of maskSsn

--- src/main/kotlin/com/hedvig/underwriter/util/TextUtil.kt
+++ src/main/kotlin/com/hedvig/underwriter/util/TextUtil.kt
@@ -3,7 +3,7 @@ package com.hedvig.underwriter.util
 import com.hedvig.underwriter.model.Market
 
 fun String.maskSsn(market: Market) = when (market) {
-    Market.SWEDEN -> this.replaceRange(this.length - 4, this.length, "XXXX")
-    Market.NORWAY -> this.replaceRange(this.length - 5, this.length, "XXXXX")
+    Market.SWEDEN,
     Market.DENMARK -> this.replaceRange(this.length - 4, this.length, "XXXX")
+    Market.NORWAY -> this.replaceRange(this.length - 5, this.length, "XXXXX")
 }
