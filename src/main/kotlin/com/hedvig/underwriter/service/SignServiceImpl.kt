f4b96c3bc2f7c93a2015dff4652fa9f2069f2b6d
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-12-11 10:56:05 +0100

    ssn with nationality

--- src/main/kotlin/com/hedvig/underwriter/service/SignServiceImpl.kt
+++ src/main/kotlin/com/hedvig/underwriter/service/SignServiceImpl.kt
@@ -24,6 +24,8 @@ import com.hedvig.underwriter.service.model.StartSignErrors
 import com.hedvig.underwriter.service.model.StartSignResponse
 import com.hedvig.underwriter.serviceIntegration.customerio.CustomerIO
 import com.hedvig.underwriter.serviceIntegration.memberService.MemberService
+import com.hedvig.underwriter.serviceIntegration.memberService.dtos.Nationality
+import com.hedvig.underwriter.serviceIntegration.memberService.dtos.SsnWithNationality
 import com.hedvig.underwriter.serviceIntegration.memberService.dtos.StartRedirectBankIdSignResponse
 import com.hedvig.underwriter.serviceIntegration.memberService.dtos.UpdateSsnRequest
 import com.hedvig.underwriter.serviceIntegration.productPricing.ProductPricingService
@@ -312,7 +314,19 @@ class SignServiceImpl(
             }
             val memberId = memberService.createMember()
 
-            memberService.updateMemberSsn(memberId.toLong(), UpdateSsnRequest(ssn = quote.data.ssn!!))
+            val ssn = quote.data.ssn!!
+            memberService.updateMemberSsn(memberId.toLong(), UpdateSsnRequest(
+                ssn = ssn,
+                ssnWithNationality = SsnWithNationality(
+                    ssn,
+                    //TODO let's fix this properly with the strategy
+                    when (ssn.length) {
+                        10 -> Nationality.DENMARK
+                        11 -> Nationality.NORWAY
+                        12 -> Nationality.SWEDEN
+                        else -> throw RuntimeException("Can't get nationality from ssn")
+                    }
+                )))
 
             return Either.Right(quoteRepository.update(quote.copy(memberId = memberId)))
         } else {
