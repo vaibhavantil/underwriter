7e4201a7636c65b3f113a5f60f63dd1c48b0913c
Fredrik Bystam <fredrik.bystam@gmail.com>
2021-01-08 15:45:16 +0100

    set explicit locale for embedded postgres, fixing tests on big sur (#181)
    * set explicit locale for embedded postgres, fixing tests on big sur
    
    * try to make it work on Linux again
    
    * printStackTrace instead of println
    
    * don't catch unnecessarily

--- src/test/kotlin/com/hedvig/underwriter/testhelp/EmbeddedPostgresSingleton.kt
+++ src/test/kotlin/com/hedvig/underwriter/testhelp/EmbeddedPostgresSingleton.kt
@@ -1,14 +1,19 @@
 package com.hedvig.underwriter.testhelp
 
 import com.opentable.db.postgres.embedded.EmbeddedPostgres
+import org.apache.commons.lang.SystemUtils
 
 object EmbeddedPostgresSingleton {
     private var _embeddedPostgres: EmbeddedPostgres? = null
     val embeddedPostgres: EmbeddedPostgres
         get() {
             if (_embeddedPostgres == null) {
-                _embeddedPostgres = EmbeddedPostgres.builder()
-                    .start()
+                val builder = EmbeddedPostgres.builder()
+                if (SystemUtils.IS_OS_MAC) {
+                    // https://github.com/zonkyio/embedded-postgres/issues/11
+                    builder.setLocaleConfig("locale", "en_US")
+                }
+                _embeddedPostgres = builder.start()
             }
 
             return _embeddedPostgres!!
