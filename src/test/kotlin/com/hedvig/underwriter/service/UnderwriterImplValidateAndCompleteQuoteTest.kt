a6642580ebbe96b75a1f2438b334ecd035379820
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2021-03-15 13:37:53 +0100

    created UnderwriterImplValidateAndCompleteQuoteTest

---
+++ src/test/kotlin/com/hedvig/underwriter/service/UnderwriterImplValidateAndCompleteQuoteTest.kt
@@ -0,0 +1,131 @@
+package com.hedvig.underwriter.service
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.hedvig.underwriter.model.QuoteData
+import com.hedvig.underwriter.service.guidelines.BaseGuideline
+import com.hedvig.underwriter.service.quoteStrategies.QuoteStrategyService
+import com.hedvig.underwriter.testhelp.databuilder.QB
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+
+class UnderwriterImplValidateAndCompleteQuoteTest {
+
+    val quoteStrategyService: QuoteStrategyService = mockk()
+
+    val cut = UnderwriterImpl(
+        mockk(relaxed = true),
+        quoteStrategyService,
+        mockk(relaxed = true)
+    )
+
+    @Test
+    fun `FailsShouldSkipAfter should only return error of FailsShouldSkipAfter`() {
+        every { quoteStrategyService.getAllGuidelines(someQuote) } returns setOf(
+            FailsShouldSkipAfter,
+            FailsShouldNotSkipAfter
+        )
+
+        val result = cut.validateGuidelines(someQuote)
+
+        assertThat(result).isEqualTo(listOf(FailsShouldSkipAfter.breachedGuideline))
+    }
+
+    @Test
+    fun `SuccessShouldSkipAfter followed by FailsShouldNotSkipAfter and FailsShouldSkipAfter and FailsShouldSkipAfter should return FailsShouldNotSkipAfter and FailsShouldSkipAfter`() {
+        every { quoteStrategyService.getAllGuidelines(someQuote) } returns setOf(
+            SuccessShouldSkipAfter,
+            FailsShouldNotSkipAfter,
+            FailsShouldSkipAfter
+        )
+
+        val result = cut.validateGuidelines(someQuote)
+
+        assertThat(result).isEqualTo(
+            listOf(
+                FailsShouldNotSkipAfter.breachedGuideline,
+                FailsShouldSkipAfter.breachedGuideline
+            )
+        )
+    }
+
+    @Test
+    fun `SuccessShouldSkipAfter and SuccessShouldNotSkipAfter should return empty list`() {
+        every { quoteStrategyService.getAllGuidelines(someQuote) } returns setOf(
+            SuccessShouldSkipAfter,
+            SuccessShouldNotSkipAfter
+        )
+
+        val result = cut.validateGuidelines(someQuote)
+
+        assertThat(result).isEqualTo(emptyList())
+    }
+
+    @Test
+    fun `two FailsShouldNotSkipAfter should return two FailsShouldNotSkipAfter`() {
+        every { quoteStrategyService.getAllGuidelines(someQuote) } returns setOf(
+            FailsShouldNotSkipAfter,
+            FailsShouldSkipAfter
+        )
+
+        val result = cut.validateGuidelines(someQuote)
+
+        assertThat(result).isEqualTo(
+            listOf(
+                FailsShouldNotSkipAfter.breachedGuideline,
+                FailsShouldSkipAfter.breachedGuideline
+            )
+        )
+    }
+
+    companion object {
+        val someQuote = QB().build()
+    }
+}
+
+object FailsShouldNotSkipAfter : BaseGuideline<QuoteData> {
+    override val breachedGuideline = this::class.simpleName!!
+
+    override val skipAfter: Boolean
+        get() = false
+
+    override val validate = { data: QuoteData ->
+        true
+    }
+}
+
+object FailsShouldSkipAfter : BaseGuideline<QuoteData> {
+    override val breachedGuideline = this::class.simpleName!!
+
+    override val skipAfter: Boolean
+        get() = true
+
+    override val validate = { data: QuoteData ->
+        true
+    }
+}
+
+object SuccessShouldSkipAfter : BaseGuideline<QuoteData> {
+    override val breachedGuideline = this::class.simpleName!!
+
+    override val skipAfter: Boolean
+        get() = true
+
+    override val validate = { data: QuoteData ->
+        false
+    }
+}
+
+object SuccessShouldNotSkipAfter : BaseGuideline<QuoteData> {
+    override val breachedGuideline = this::class.simpleName!!
+
+    override val skipAfter: Boolean
+        get() = false
+
+    override val validate = { data: QuoteData ->
+        false
+    }
+}
+
+
