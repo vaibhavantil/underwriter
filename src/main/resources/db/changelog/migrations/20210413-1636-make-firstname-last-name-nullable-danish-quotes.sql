96bda8eb3ef485320b2bc23dbd5ef999b279e8e6
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2021-04-14 17:24:27 +0200

    update rollback statement

--- src/main/resources/db/changelog/migrations/20210413-1636-make-firstname-last-name-nullable-danish-quotes.sql
+++ src/main/resources/db/changelog/migrations/20210413-1636-make-firstname-last-name-nullable-danish-quotes.sql
@@ -16,9 +16,9 @@ ALTER TABLE quote_revision_danish_travel_data
 ALTER TABLE quote_revision_danish_travel_data
     ALTER COLUMN last_name DROP NOT NULL;
 
---rollback ALTER TABLE quote_revision_danish_home_contents_data ALTER COLUMN first_name NOT NULL;
---rollback ALTER TABLE quote_revision_danish_home_contents_data ALTER COLUMN last_name NOT NULL;
---rollback ALTER TABLE quote_revision_danish_accident_data ALTER COLUMN first_name NOT NULL;
---rollback ALTER TABLE quote_revision_danish_accident_data ALTER COLUMN last_name NOT NULL;
---rollback ALTER TABLE quote_revision_danish_travel_data ALTER COLUMN first_name NOT NULL;
---rollback ALTER TABLE quote_revision_danish_travel_data ALTER COLUMN last_name NOT NULL;
+--rollback ALTER TABLE quote_revision_danish_home_contents_data ALTER COLUMN first_name SET NOT NULL;
+--rollback ALTER TABLE quote_revision_danish_home_contents_data ALTER COLUMN last_name SET NOT NULL;
+--rollback ALTER TABLE quote_revision_danish_accident_data ALTER COLUMN first_name SET NOT NULL;
+--rollback ALTER TABLE quote_revision_danish_accident_data ALTER COLUMN last_name SET NOT NULL;
+--rollback ALTER TABLE quote_revision_danish_travel_data ALTER COLUMN first_name SET NOT NULL;
+--rollback ALTER TABLE quote_revision_danish_travel_data ALTER COLUMN last_name SET NOT NULL;
