44bea9f07944e41f70c3bbc6e16fa8cf37294bca
vonElfvin <elvin.granat@gmail.com>
2021-04-28 15:39:12 +0200

    Remove unused dtos

--- src/main/kotlin/com/hedvig/underwriter/web/dtos/ChooseActiveFromDto.kt
+++
@@ -1,7 +0,0 @@
-package com.hedvig.underwriter.web.dtos
-
-import java.time.LocalDateTime
-
-data class ChooseActiveFromDto(
-    val activeFrom: LocalDateTime
-)
--- src/main/kotlin/com/hedvig/underwriter/web/dtos/IncompleteQuoteResponseDto.kt
+++
@@ -1,7 +0,0 @@
-package com.hedvig.underwriter.web.dtos
-
-import java.util.UUID
-
-data class IncompleteQuoteResponseDto(
-    val id: UUID
-)
